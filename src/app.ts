c7b728b3f47dff6b00317b5d24934134eec67247
chiscookeke11 <chiscookeke11@gmail.com>
2026-03-27 13:39:39 +0100

    updates

--- src/app.ts
+++ src/app.ts
@@ -61,15 +61,12 @@ const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS
 
 const corsOptions: cors.CorsOptions = {
   origin: (origin, callback) => {
-    // Allow non-browser tools like curl/postman
     if (!origin) {
       return callback(null, true);
     }
-
     if (allowedOrigins.includes(origin)) {
       return callback(null, true)
     }
-
     return callback(new Error("Not allowed by CORS"));
   },
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
